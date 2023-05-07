3736ff827e5d38c2606cecda97d5d1d8e99f6ef8
Mateus Meneses Rocha <contato.menesesdev@gmail.com>
2023-05-07 13:01:57 -0300

    Iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Text('Dadoos'),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

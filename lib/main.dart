4feff06fca4f2f306286a334f3a30c69d7bedd6e
Mateus Meneses Rocha <contato.menesesdev@gmail.com>
2023-05-07 19:14:06 -0300

    Restruturando o código para adicionar um texto em baixo dos dados

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal.shade700,
         appBar: AppBar(
-          title: Text('Dadoos'),
-          backgroundColor: Colors.teal.shade900,
+          elevation: 0.0,
+          title: Text('Role os dados!'),
+          backgroundColor: Colors.teal.shade700,
         ),
         body: Dadoos(),
       ),
@@ -20,34 +21,47 @@ class Dadoos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var numDadoEsquerdo = 1;
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextButton(
-                onPressed: () {},
-                child: Image.asset("imagens/dado$numDadoEsquerdo.png"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Image.asset("imagens/dado$numDadoEsquerdo.png"),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            width: 1,
-            height: 100,
-            color: Colors.white,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextButton(
-                onPressed: () {},
-                child: Image.asset("imagens/dado1.png"),
+              Container(
+                width: 1,
+                height: 100,
+                color: Colors.white,
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Image.asset("imagens/dado1.png"),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "toque na imagem do dado para mudar a face dele",
+          style: TextStyle(color: Colors.white),
+        )
+      ],
     );
   }
 }
